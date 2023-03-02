47357461dff06536205c274e7427326980722050
AlekasndrHavryliuk <116018047+AnKulai@users.noreply.github.com>
2023-03-02 02:11:45 +0200

    Test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import Section from './Section/Section';
 const App = () => {
   console.log(process.env.REACT_APP_TEST);
   console.log(process.env);
+  console.log(1231);
   return (
     <>
       <Section title="PhoneBook">
